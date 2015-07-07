52202b294ffd0eafa56c772ebe7f45f81d8035c5
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-07-07 00:02:50 -0500

    Add missing file

---
+++ dnscheck.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+type DnsCheck struct {
+	Domain string
+	Ip     string
+}
+
+func ParseDnsCheck(data map[string]interface{}) DnsCheck {
+	check := DnsCheck{}
+
+	if data["domain"] != nil {
+		check.Domain = data["domain"].(string)
+	}
+
+	if data["ip"] != nil {
+		check.Ip = data["ip"].(string)
+	}
+
+	return check
+}
+
+func (check DnsCheck) Perform() error {
+	if check.Domain == "" {
+		return fmt.Errorf("Domain should not be empty")
+	}
+
+	if check.Ip == "" {
+		return fmt.Errorf("IP should not be empty")
+	}
+
+	log.Printf("Performing DNS check for domain=%v ip=%v\n", check.Domain, check.Ip)
+
+	addr, err := net.ResolveIPAddr("ip4:icmp", check.Domain)
+	if err != nil {
+		return err
+	}
+
+	if addr.String() != check.Ip {
+		return fmt.Errorf("Expected IP: %v, got: %v", check.Ip, addr.String())
+	}
+
+	return nil
+}
